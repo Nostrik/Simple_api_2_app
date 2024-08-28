6b96fedbaa53842ed8a533730fc44c703ff58d88
unknown <sairus.89@mail.com>
2024-08-28 20:52:14 +0300

    logger add db connetct problem

--- simple_api_2/database.py
+++ simple_api_2/database.py
@@ -2,8 +2,8 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+asyncpg://admin:admin@db:5432/simple_api_2_db"  # Замените username, password и dbname на свои значения
-DATABASE_URL_2 = "postgresql+asyncpg://admin:admin@db:5432"
+DATABASE_URL = "postgresql+asyncpg://admin:admin@python_advanced_diploma-db-1:5432/simple_api_2_db"  # Замените username, password и dbname на свои значения
+# DATABASE_URL_2 = "postgresql+asyncpg://admin:admin@db:5432"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
--- simple_api_2/main.py
+++ simple_api_2/main.py
@@ -1,28 +1,37 @@
+import sys
+import os
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from pydantic import BaseModel
 from datetime import datetime
+from loguru import logger
 from .database import AsyncSessionLocal, engine, Base
 from .models import User
+from .sсhemas import UserCreate
 
 app = FastAPI()
-
-
-class UserCreate(BaseModel):
-    username: str
-    created_at: datetime = None
+logger.remove()
+# logger.add(sys.stdout,format="{level}:    {message}" , colorize=True)
+logger.add(sys.stderr, level="INFO", format="<green>{level}</green>:     {message}", colorize=True)
+tables_created = False
 
 
 # Создание таблиц в базе данных (если они еще не созданы)
 @app.on_event("startup")
 async def startup():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+    global tables_created
+    logger.debug(f"Create tables called in process ID: {os.getpid()}")
+    if not tables_created:
+        logger.info("Create tables")
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+        tables_created = True
 
 
 # Функция для получения асинхронной сессии базы данных
 async def get_db() -> AsyncSession:
+    logger.info("Get AsyncSession")
     async with AsyncSessionLocal() as session:
         yield session
 
